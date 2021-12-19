4daacacbc3174ab119356f47ea8e833fa470b849
Ankur <ankurdwivedi75@gmail.com>
2021-12-19 21:12:50 +0530

    added dashboard, login & logout function

--- organization/application/src/App.js
+++ organization/application/src/App.js
@@ -1,23 +1,25 @@
+import {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import doctorImg from './doctor.png';
 import patientImg from './patient.png';
 import LoginForm from "./Login.js";
+import Dashboard from "./dashboard";
 
 function App() {
 
+    const[user, setUser] = useState("");
 
-    return (
-      <div>
-          <div className={"lContainer"}>
-              <LoginForm title={"Login as doctor"} img={doctorImg}/>
-          </div>
-          <div className={"lContainer"}>
-              <LoginForm title={"Login as device"} img={patientImg}/>
-          </div>
-      </div>
-
-    );
+    return user ?
+        <Dashboard id={user} setUser={setUser}/> :
+        <div>
+            <div className={"lContainer"}>
+                <LoginForm title={"Login as doctor"} img={doctorImg} setUser={setUser}/>
+            </div>
+            <div className={"lContainer"}>
+                <LoginForm title={"Login as device"} img={patientImg} setUser={setUser}/>
+            </div>
+        </div>;
 }
 
 export default App;
--- organization/application/src/Login.js
+++ organization/application/src/Login.js
@@ -1,38 +1,42 @@
 import React, { Component } from 'react';
 import {Button, Form, Input, Checkbox} from 'antd';
 import {UserOutlined, LockOutlined} from '@ant-design/icons';
+const FormItem = Form.Item;
 
 class LoginForm extends Component {
 
-	render() {
+	handleSubmit = values => {
+		this.props.setUser(values.username);
+	}
 
+	render() {
 
 		return (
 			<div className={"lItem"}>
 				<img className={"loginImage"} src={this.props.img} alt={this.props.title} width="100" height="100"/>
 				<div className={"loginForm"}>
 					<h2>{this.props.title}</h2>
-					<Form onSubmit={this.props.onSubmit} className={"login-form"}>
-						<Form.Item
+					<Form onFinish={this.handleSubmit} className={"login-form"}>
+						<FormItem
 							name={"username"}
 							rules= {[{
 								required: true,
 								message: 'Please input your username!'
 							}]}>
 							<Input prefix={<UserOutlined/>} placeholder="Username"/>
-						</Form.Item>
-						<Form.Item
+						</FormItem>
+						<FormItem
 							name={"password"}
 							rules={[{
 								required: true,
 								message: 'Please enter your password'
 							}]}>
 							<Input prefix={<LockOutlined/>} type={"password"} placeholder={"Password"}/>
-						</Form.Item>
-						<Form.Item name={"remember"} valuePropName={"checked"} initialValue={true}>
+						</FormItem>
+						<FormItem name={"remember"} valuePropName={"checked"} initialValue={true}>
 							<Checkbox>Remember me</Checkbox>
 							<Button className={"login-form-button"} type={"primary"} htmlType={"submit"}>Log in</Button>
-						</Form.Item>
+						</FormItem>
 					</Form>
 				</div>
 			</div>
